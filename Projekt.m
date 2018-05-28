8cfa84abaf003e6df716c7e75ae1d563961083f8
KarolGa <38383180+KarolGa@users.noreply.github.com>
2018-05-28 17:23:38 +0200

    WykrywanieRuchu

--- Projekt.m
+++ Projekt.m
@@ -18,7 +18,7 @@ vidobj = videoinput('winvideo',1,'YUY2_640x480');
 %% Krok 3 :Konfiguracja w³asnoœci obiektu (opcjonalnie)
 % Step 3: Configure image acquisition object properties (Optional)
  get(vidobj)
- inspect(vidobj)
+% inspect(vidobj)
  set(vidobj,'ReturnedColorSpace','rgb')
 %%
 % config = triggerinfo(vidobj)
@@ -30,16 +30,47 @@ set(vidobj,'TriggerRepeat', Inf);
 %% Krok 4: Akwizycja danych wizyjnych - obrazu
 % Step 4: Acquire Image Data
 start(vidobj);
-for i=1:9
+disp('Zacznij wykonywac gest');
+pause(0.2);
+for i=1:8
  trigger(vidobj);
- pause(0.1)
- im = getdata(vidobj,1);
- photos(:,:,:,i)=im;
- figure,imshow(photos(:,:,:,i));
+ %pause(0.1)
+  photos(:,:,:,i) = getdata(vidobj,1);
+ subplot(2, 4, i);
+ imshow(photos(:,:,:,i));
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Krok 5 : Usuniêcie obiektu z pamiêci i wyczyszczenie przestrzeni roboczej
 % Step 5 : Clean Up
-stop(vidobj),
-delete(vidobj),
+stop(vidobj);
+delete(vidobj);
 clear vidobj;
+
+%% Krok 6 : Sprawdzanie czy piksel siê zmieni³
+for i=1:8
+bwPhotos(:,:,i)=rgb2gray(photos(:,:,:,i));
+figure(2);
+subplot(2, 4, i);
+imshow(bwPhotos(:,:,i));
+end
+
+senseBwPhotos=bwPhotos;
+
+figure(3);
+subplot(2, 4, 1);
+imshow(bwPhotos(:,:,1));
+for i=2:length(bwPhotos(1,1,:))
+    for j=1:length(bwPhotos(:,1,1))
+        for k=1:length(bwPhotos(1,:,1))
+        if(bwPhotos(j,k,i)>(bwPhotos(j,k,i-1))+10 | bwPhotos(j,k,i)<(bwPhotos(j,k,i-1))-10)
+        senseBwPhotos(j,k,i)=255;
+        else
+        senseBwPhotos(j,k,i)=bwPhotos(j,k,i); 
+        end
+        end
+    end
+subplot(2, 4, i);
+imshow(senseBwPhotos(:,:,i));
+end
+
+%Filtracja 
