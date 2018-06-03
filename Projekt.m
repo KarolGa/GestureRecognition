249aec0e1daae9843fd2a004148b07a5208489b8
KarolGa <38383180+KarolGa@users.noreply.github.com>
2018-06-03 14:52:34 +0200

    Filtracja, binaryzacja

--- Projekt.m
+++ Projekt.m
@@ -17,9 +17,9 @@ vidobj = videoinput('winvideo',1,'YUY2_640x480');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Krok 3 :Konfiguracja w³asnoœci obiektu (opcjonalnie)
 % Step 3: Configure image acquisition object properties (Optional)
- get(vidobj)
+ get(vidobj);
 % inspect(vidobj)
- set(vidobj,'ReturnedColorSpace','rgb')
+ set(vidobj,'ReturnedColorSpace','rgb');
 %%
 % config = triggerinfo(vidobj)
 % triggerconfig(vidobj,config(2))
@@ -32,7 +32,8 @@ set(vidobj,'TriggerRepeat', Inf);
 start(vidobj);
 disp('Zacznij wykonywac gest');
 pause(0.2);
-for i=1:8
+numPh=8; %liczba zdjêæ wykonanych przez kamerke
+for i=1:numPh
  trigger(vidobj);
  %pause(0.1)
   photos(:,:,:,i) = getdata(vidobj,1);
@@ -47,30 +48,47 @@ delete(vidobj);
 clear vidobj;
 
 %% Krok 6 : Sprawdzanie czy piksel siê zmieni³
-for i=1:8
+for i=1:numPh
 bwPhotos(:,:,i)=rgb2gray(photos(:,:,:,i));
 figure(2);
 subplot(2, 4, i);
 imshow(bwPhotos(:,:,i));
 end
 
-senseBwPhotos=bwPhotos;
+sensePhotos=bwPhotos;
 
 figure(3);
 subplot(2, 4, 1);
 imshow(bwPhotos(:,:,1));
-for i=2:length(bwPhotos(1,1,:))
+for i=2:numPh
     for j=1:length(bwPhotos(:,1,1))
         for k=1:length(bwPhotos(1,:,1))
         if(bwPhotos(j,k,i)>(bwPhotos(j,k,i-1))+10 | bwPhotos(j,k,i)<(bwPhotos(j,k,i-1))-10)
-        senseBwPhotos(j,k,i)=255;
+        sensePhotos(j,k,i)=255;
         else
-        senseBwPhotos(j,k,i)=bwPhotos(j,k,i); 
-        end
+        sensePhotos(j,k,i)=bwPhotos(j,k,i);         end
         end
     end
 subplot(2, 4, i);
-imshow(senseBwPhotos(:,:,i));
+imshow(sensePhotos(:,:,i));
 end
 
-%Filtracja 
+%% Krok 7 : Binaryzacja i filtracja 
+
+
+seO=strel('disk',7);
+seC=strel('disk',80);
+figure(4)
+for i=2:numPh
+    filPhotos(:,:,i)=255*imopen(sensePhotos(:,:,i)==255, seO);
+    filPhotos(:,:,i)=imclose(filPhotos(:,:,i), seC);
+
+    subplot(2, 4, i);
+    imshow(filPhotos(:,:,i));
+end
+
+%% Krok 8 : Analiza obrazow
+for i=2:numPh
+[imLb, num] = bwlabel(filPhotos(:,:,i), 8);
+prop=regionprops(imLb, 'Centroid')
+end
