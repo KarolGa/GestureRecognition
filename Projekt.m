be306e30159b73dda3a0ceb5486bae34607385b6
Artur Bębenek <arturbebenek96@gmail.com>
2018-06-12 01:09:05 +0200

    Update Projekt.m

--- Projekt.m
+++ Projekt.m
@@ -1,8 +1,9 @@
+
 close all;
 clear all;
 clc;
 
-%% Krok 1 : Pozyskaj informacje o sprzêcie
+%% Krok 1 : Pozyskaj informacje o sprz?cie
 % Adaptor name
 % Device ID
 % Video format
@@ -11,11 +12,11 @@ clc;
 %dev_info = imaqhwinfo('winvideo',1)
 %dev_info.SupportedFormats;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Krok 2 : Utwórz wejœciowy obiekt akwizycji
+%% Krok 2 : Utw?rz wej?ciowy obiekt akwizycji
 % Step2 : Create a video input object
 vidobj = videoinput('winvideo',1,'YUY2_640x480');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Krok 3 :Konfiguracja w³asnoœci obiektu (opcjonalnie)
+%% Krok 3 :Konfiguracja w?asno?ci obiektu (opcjonalnie)
 % Step 3: Configure image acquisition object properties (Optional)
  get(vidobj);
 % inspect(vidobj)
@@ -32,7 +33,7 @@ set(vidobj,'TriggerRepeat', Inf);
 start(vidobj);
 disp('Zacznij wykonywac gest');
 pause(0.2);
-numPh=8; %liczba zdjêæ wykonanych przez kamerke
+numPh=8; %liczba zdj?? wykonanych przez kamerke
 for i=1:numPh
  trigger(vidobj);
  %pause(0.1)
@@ -41,13 +42,13 @@ for i=1:numPh
  imshow(photos(:,:,:,i));
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Krok 5 : Usuniêcie obiektu z pamiêci i wyczyszczenie przestrzeni roboczej
+%% Krok 5 : Usuni?cie obiektu z pami?ci i wyczyszczenie przestrzeni roboczej
 % Step 5 : Clean Up
 stop(vidobj);
 delete(vidobj);
 clear vidobj;
 
-%% Krok 6 : Sprawdzanie czy piksel siê zmieni³
+%% Krok 6 : Sprawdzanie czy piksel si? zmieni?
 for i=1:numPh
 bwPhotos(:,:,i)=rgb2gray(photos(:,:,:,i));
 figure(2);
@@ -88,7 +89,52 @@ for i=2:numPh
 end
 
 %% Krok 8 : Analiza obrazow
+gesture_x=[];
+gesture_y=[];
+flag_error=0;
 for i=2:numPh
 [imLb, num] = bwlabel(filPhotos(:,:,i), 8);
-prop=regionprops(imLb, 'Centroid')
+if num ~= 1
+    disp('gest nierozpoznany');
+    flag_error=1;
+    break;
+else    
+    %miejsce dla rozpoznawania najwiekszego obiektu gdy num>1
+  %  for i=1:num
+%fig = ismember(imLb, i);
+
+prop(i)=regionprops(imLb, 'Centroid')
+    if i>2
+     if (prop(i).Centroid(1)>(prop(i-1).Centroid(1))) 
+       gesture_x(i)=1;
+     else
+       gesture_x(i)=-1;
+       end
+         if (prop(i).Centroid(2)>(prop(i-1).Centroid(2))) 
+              gesture_y(i)=1;
+         else
+             gesture_y(i)=-1; 
+         end
+     
+    end
+    if i == numPh
+      xdiff=abs(prop(i).Centroid(1)-(prop(2).Centroid(1)));
+      ydiff=abs(prop(i).Centroid(2)-(prop(2).Centroid(2)));
+    end
+end
+end
+%%
+if flag_error ~=1
+  if (sum(gesture_x)>0 && xdiff>ydiff)
+   disp('gesture_1, hand> left');
+  end
+        if (sum(gesture_x)<0 && xdiff>ydiff)
+         disp('gesture_2, hand> right');
+        end
+               if (sum(gesture_y)<0 && xdiff<ydiff)
+                disp('gesture_3, hand> up');
+               end
+                    if (sum(gesture_y)>0 && xdiff<ydiff)
+                      disp('gesture_4, hand> down');
+                    end
 end
